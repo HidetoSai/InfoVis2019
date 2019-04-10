57875e2adf7f124a604dfa29fc3fc3c07e269ac8
HidetoSai <e1211@s.akashi.ac.jp>
2019-04-10 17:33:19 +0900

    2nd commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,48 +1,40 @@
-Vec3 = function( x, y, z )
-{
+Vec3 = function(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
 }
 
-Vec3.prototype.add = function( v )
-{
+Vec3.prototype.add = function(v) {
     this.x += v.x;
     this.y += v.y;
     this.z += v.z;
     return this;
 }
 
-Vec3.prototype.sub = function( v )
-{
+Vec3.prototype.sub = function(v) {
     this.x -= v.x;
     this.y -= v.y;
     this.z -= v.z;
     return this;
 }
 
-Vec3.prototype.sum = function()
-{
+Vec3.prototype.sum = function() {
     return this.x + this.y + this.z;
 }
 
-Vec3.prototype.min = function()
-{
+Vec3.prototype.min = function() {
     return Math.min( this.x, this.y, this.z );
 }
 
-Vec3.prototype.max = function()
-{
+Vec3.prototype.max = function() {
     return Math.max( this.x, this.y, this.z );
 }
 
-Vec3.prototype.mid = function()
-{
+Vec3.prototype.mid = function() {
     return this.sum() - this.min() - this.max();
 }
 
-Vec3.prototype.cross = function( v )
-{
+Vec3.prototype.cross = function(v) {
     var x = this.x, y = this.y, z = this.z;
     this.x = y * v.z - z * v.y;
     this.y = z * v.x - x * v.z;
@@ -50,7 +42,6 @@ Vec3.prototype.cross = function( v )
     return this;
 }
 
-Vec3.prototype.length = function()
-{
-    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+Vec3.prototype.length = function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
 }
